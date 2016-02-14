d1922d49fdd6aa54411916cd3ea1b0a07c7bd4c1
apegrave <apegrave@gmail.com>
2016-02-14 20:14:59 +0300

    132
    2

--- style.js
+++ style.js
@@ -2,21 +2,6 @@
 
 $(function() {
     
-    //configuretion
-    var width = 720;
-    var animationSpeed = 1000;
-    var pause = 3000;
-    var currentSlide = 1;
-    
-    //cache DOM
-    var $sliderr = $('#sliderr');
-    var $slideeContainer = $sliderr.find('.slidess');
-    var $slidess = $slideeContainer.find('.slidee');
-    
-    
-    setInterval(function() {
-        $slideeContainer.animate({'margin-left': '-='+width}, animationSpeed
-function() { currentSlide++; if (currentSlide === $slidee.length) { currentSlide = 1; $SlideeContainer.css('margin-left', 0); });
-    }, pause);
+$('#sliderr').hide(1000);
     
 })
