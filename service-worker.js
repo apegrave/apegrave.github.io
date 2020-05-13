6a0a5871119cec8a466140edf7538dfe20f67ed6
apegrave <apegrave@gmail.com>
2020-05-13 20:08:34 +0300

    123
    123

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.eebb6c0390ad329ebbf1120bf5b4c773.js"
+  "/precache-manifest.0d9a16108c310b2171ac9ce4480dfb47.js"
 );
 
 self.addEventListener('message', (event) => {
